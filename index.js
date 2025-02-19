4676165d09f685117ac435c8e22448c04c1fa6cd
alfredo0117 <146121376+alfredo0117@users.noreply.github.com>
2025-02-19 17:58:01 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+function comprobar() {
+
+switch (document.getElementById('signo').value){
+    case "capricornio" :
+        alert('tendras un hijo esta semana') ;
+        break ;
+    case "sagitario" :
+        alert('ganaras mucho al fortnite') ;
+        break ;
+    case "aries" :
+        alert('te van a regalar el animal que quieres a lo largo de este mes') ;
+        break ; 
+    case "tauro" : 
+          alert('el karma te llegara pronto y no será bueno, si eres mi padre te pasaran cosas buenas') ;
+          break ; 
+    case "escorpio" :
+        alert('si eres rafa conseguiras todo lo que quieres en tu vida y si eres isa tambien, si no lo eres, espero que te mueras rata') ;
+        break ; 
+    case "piscis" :
+        alert('no se que poner pero ojala no te pase lo que quieras que te pase') ;
+        break ; 
+    case "virgo" :
+        alert('la mejor para ti siempre y la mala en ninguna') ;
+        break ;
+    case "géminis" :
+        alert('si no eres carlo eres un retrasado entonces y no me importa quien seas') ;
+        break ; 
+    case "cáncer" :
+        alert('vaya puto nombre del zodiaco de puta mierda pero ojala no te de cancer en verda <^^> fortnite') ;
+        break ; 
+    case "leo" :
+        alert('que lees? PUAAAAA no se bro eres un pringado que te cagas creo si eres leo y no lees') ; 
+        break ; 
+    case "libra" : 
+        alert('los libras no se como son pero tiene toda la pinta de que me caen como el culo y ser prepotentes asi que nada bueno para ti') ;
+        break ; 
+    case "acuario":
+        alert('pues nada está guapo el nombre y tal pero tiene pinta de ser un signo aburrido de cojones sinceramente') ; 
+        break ;
+    }
+
+}
